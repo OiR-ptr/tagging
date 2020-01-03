e5649f42deb551134215567dc7051f9fa5f0f1fc
OiR-ptr <myr.ryusuke@gmail.com>
2020-01-03 22:51:37 +0900

    :construction: add vanilla router
    通常のReact-routerを用いたページングテスト動作

--- src/main/EntryPoint.jsx
+++ src/main/EntryPoint.jsx
@@ -1,14 +1,25 @@
 /* eslint-disable */
 
 import React from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import App from '../components/App';
 
 const entry = () => {
     return (
-        <div>
-            A happy new year
-            <App />
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/" exact >
+                    <App />
+                </Route>
+                <Route path="/about" >
+                    <div>
+                        A happy new year!!
+                    </div>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
