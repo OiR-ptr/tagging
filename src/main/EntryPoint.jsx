470be1f69cb10b909e674be2df3d05c134710705
OiR-ptr <myr.ryusuke@gmail.com>
2020-01-03 22:43:35 +0900

    :cry: Remove react-router
    react-router周りが変だったのでいったん全消しした

--- src/main/EntryPoint.jsx
+++ src/main/EntryPoint.jsx
@@ -1,18 +1,7 @@
 /* eslint-disable */
 
 import React from 'react';
-import {createBrowserHistory} from "history";
-import {routerReducer, routerMiddleware, ConnectedRouter} from "react-router-redux";
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from "react-redux";
 import App from '../components/App';
-
-const historyObject = createBrowserHistory();
-const middleware = routerMiddleware(historyObject);
-const store = createStore(routerReducer, applyMiddleware(middleware));
-
-console.log(historyObject);
-console.log(store);
 
 const entry = () => {
     return (
