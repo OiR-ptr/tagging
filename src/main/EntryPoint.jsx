d7b3d77d1e6617e49a155abc69e492728ab32d64
OiR-ptr <myr.ryusuke@gmail.com>
2020-02-02 22:31:22 +0900

    :wrench: prettier
    Prettierを導入

--- src/main/EntryPoint.jsx
+++ src/main/EntryPoint.jsx
@@ -1,58 +1,61 @@
 /* eslint-disable */
 
-import React from 'react';
-import { createMemoryHistory } from 'history';
-import { combineReducers, applyMiddleware, compose, createStore } from 'redux';
-import { connectRouter, routerMiddleware, ConnectedRouter } from 'connected-react-router';
-import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { createMemoryHistory } from "history";
+import { combineReducers, applyMiddleware, compose, createStore } from "redux";
+import {
+  connectRouter,
+  routerMiddleware,
+  ConnectedRouter
+} from "connected-react-router";
+import { MemoryRouter as Router, Switch, Route } from "react-router-dom";
 
-import App from '../components/App';
-import { Provider } from 'react-redux';
+import App from "../components/App";
+import { Provider } from "react-redux";
 
-const createRootReducer = (history) => combineReducers({
-    router: connectRouter(history),
+const createRootReducer = history =>
+  combineReducers({
+    router: connectRouter(history)
     // rest of your reducers
-});
+  });
 
 const history = createMemoryHistory();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const configureStore = (preloadedState) => {
-    const store = createStore(
-        createRootReducer(history),
-        preloadedState,
-        composeEnhancers(
-            applyMiddleware(
-                routerMiddleware(history),
-                // other middlewares
-            )
-        )
-    );
-    return store;
-}
+const configureStore = preloadedState => {
+  const store = createStore(
+    createRootReducer(history),
+    preloadedState,
+    composeEnhancers(
+      applyMiddleware(
+        routerMiddleware(history)
+        // other middlewares
+      )
+    )
+  );
+  return store;
+};
 
-const store = configureStore( /* provide initial state if any */ );
+const store = configureStore(/* provide initial state if any */);
 
 const entry = () => {
-    return (
-        <Provider store={store}>
-            <ConnectedRouter history={history}>
-                <Router>
-                    <Switch>
-                        <Route path="/" exact >
-                            <App />
-                        </Route>
-                        <Route path="/about" >
-                            <div>
-                                A happy new year!!
-                            </div>
-                        </Route>
-                    </Switch>
-                </Router>
-            </ConnectedRouter>
-        </Provider>
-    );
-}
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <App />
+            </Route>
+            <Route path="/about">
+              <div>A happy new year!!</div>
+            </Route>
+          </Switch>
+        </Router>
+      </ConnectedRouter>
+    </Provider>
+  );
+};
 
 export default entry;
