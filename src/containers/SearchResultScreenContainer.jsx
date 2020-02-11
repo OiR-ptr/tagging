1388ac9008c775411e34171c7875d1da99ab712e
OiR-ptr <myr.ryusuke@gmail.com>
2020-02-11 22:58:36 +0900

    :construction: add search result screen
    検索結果画面を追加

---
+++ src/containers/SearchResultScreenContainer.jsx
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import SearchResultScreen from "../components/SearchResultScreen";
+
+function mapStateToProps(state) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResultScreen);
